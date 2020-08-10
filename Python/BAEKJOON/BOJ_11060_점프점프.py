9a8c77e92414c06c2c8ded1954e0ed0bdaae9772
전상혁 <sh_jeon@kakao.com>
2020-08-10 23:44:29 +0900

    2020-08-10 23:44:29

---
+++ Python/BAEKJOON/BOJ_11060_점프점프.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('BOJ_11060.txt', 'r')
+
+N = int(input())
+data = list(map(int, input().split()))
+
+dp = [1000] * N
+for i in range(N):
+    if i == 0:
+        dp[i] = 0
+    for j in range(1, data[i]+1):
+        if i+j >= N:
+            break
+        dp[i+j] = min(dp[i+j], dp[i]+1)
+
+print(dp[N-1] if dp[N-1] != 1000 else -1)
