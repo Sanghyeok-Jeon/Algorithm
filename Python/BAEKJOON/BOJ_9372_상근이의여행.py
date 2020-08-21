dff86738670c6680996b707c7704b9c12aabc791
전상혁 <sh_jeon@kakao.com>
2020-08-21 16:03:26 +0900

    2020-08-21 16:03:25

---
+++ Python/BAEKJOON/BOJ_9372_상근이의여행.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('BOJ_9372.txt', 'r')
+
+import sys
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    N, M = map(int, sys.stdin.readline().split())
+    for _ in range(M):
+        a, b = map(int, sys.stdin.readline().split())
+
+    print(N-1)
