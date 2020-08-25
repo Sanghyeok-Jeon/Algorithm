75644c7782901f0f3f8b4ddadda10b60e6079c12
전상혁 <sh_jeon@kakao.com>
2020-08-26 00:48:15 +0900

    2020-08-26 00:48:14

---
+++ Python/BAEKJOON/BOJ_1926_그림.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(10000)
+sys.stdin = open('BOJ_1926.txt', 'r')
+
+def DFS(i, j):
+    global cnt
+
+    for mode in range(4):
+        ni = i + di[mode]
+        nj = j + dj[mode]
+        if 0 <= ni < n and 0 <= nj < m and data[ni][nj] == 1:
+            data[ni][nj] = 0
+            cnt += 1
+            DFS(ni, nj)
+
+n, m = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(n)]
+
+di = [1, -1, 0, 0]
+dj = [0, 0, 1, -1]
+
+cnt_draw = 0
+max_draw = 0
+for i in range(n):
+    for j in range(m):
+        if data[i][j]:
+            cnt = 1
+            cnt_draw += 1
+            data[i][j] = 0
+            DFS(i, j)
+            max_draw = max(max_draw, cnt)
+
+print(cnt_draw)
+print(max_draw)
