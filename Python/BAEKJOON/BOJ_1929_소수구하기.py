8eb2caeb438af9c1a0b6cad73430aed8aa900c49
Sanghyeok-Jeon <sh_jeon@kakao.com>
2020-09-23 21:02:32 +0900

    2020-09-23 21:02:32

---
+++ Python/BAEKJOON/BOJ_1929_소수구하기.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('BOJ_1929.txt', 'r')
+
+M, N = map(int, input().split())
+
+visited = [0] * (N+1)
+
+for i in range(1, N+1):
+    if i < 2:
+        continue
+
+    if not visited[i]:
+        if i >= M:
+            print(i)
+
+        for j in range(i*2, N+1, i):
+            visited[j] = 1
