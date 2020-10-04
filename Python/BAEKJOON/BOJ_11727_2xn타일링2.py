6b68c7df41d9f4c0a3708f9621e9efe48edfb373
전상혁 <sh_jeon@kakao.com>
2020-10-05 00:09:06 +0900

    2020-10-05 00:09:06

---
+++ Python/BAEKJOON/BOJ_11727_2xn타일링2.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('BOJ_11727.txt', 'r')
+
+n = int(input())
+
+dp = [0] * (n+1)
+
+for i in range(1, n+1):
+    if i == 1:
+        dp[i] = 1
+    elif i == 2:
+        dp[i] = 3
+    else:
+        dp[i] = dp[i-2] * 2 + dp[i-1]
+
+print(dp[n] % 10007)
