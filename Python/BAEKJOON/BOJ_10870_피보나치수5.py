b8dcc4619828cbeddf5385fa45e8f35acb70603d
전상혁 <sh_jeon@kakao.com>
2020-10-05 00:12:12 +0900

    2020-10-05 00:12:11

---
+++ Python/BAEKJOON/BOJ_10870_피보나치수5.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_10870.txt', 'r')
+
+n = int(input())
+
+dp = [0] * (n+1)
+
+for i in range(1, n+1):
+    if i == 1:
+        dp[i] = 1
+    else:
+        dp[i] = dp[i-2] + dp[i-1]
+
+print(dp[n])
