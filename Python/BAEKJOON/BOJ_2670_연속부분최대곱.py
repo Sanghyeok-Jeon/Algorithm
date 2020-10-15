6489410bb10d5bb481fa821972d7ee0e58ab2a9a
전상혁 <sh_jeon@kakao.com>
2020-10-15 13:41:41 +0900

    2020-10-15 13:41:41

---
+++ Python/BAEKJOON/BOJ_2670_연속부분최대곱.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open('BOJ_2670.txt', 'r')
+
+N = int(sys.stdin.readline())
+num = [float(sys.stdin.readline()) for _ in range(N)]
+
+for i in range(1, N):
+    num[i] = max(num[i], num[i] * num[i-1])
+
+print("%.3f" % max(num))
