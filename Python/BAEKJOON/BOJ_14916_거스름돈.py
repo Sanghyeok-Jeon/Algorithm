469557d5c44cc4eb601755ae32c6a96bb493f9b6
전상혁 <sh_jeon@kakao.com>
2020-10-29 10:11:13 +0900

    2020-10-29 10:11:13

---
+++ Python/BAEKJOON/BOJ_14916_거스름돈.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_14916.txt', 'r')
+
+n = int(input())
+
+answer = 0
+if n == 1 or n == 3:
+    answer = -1
+elif (n % 5) % 2 == 0:
+    answer = (n // 5) + (n % 5) // 2
+else:
+    answer = (n // 5 - 1) + (n % 5 + 5) // 2
+
+print(answer)
