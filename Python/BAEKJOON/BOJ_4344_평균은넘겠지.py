0c229d473b5b52d75ea2a1361a9166b40cebf2a3
전상혁 <sh_jeon@kakao.com>
2020-11-13 19:56:03 +0900

    2020-11-13 19:56:03

---
+++ Python/BAEKJOON/BOJ_4344_평균은넘겠지.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('BOJ_4344.txt', 'r')
+
+C = int(input())
+
+for _ in range(C):
+    data = list(map(int, input().split()))
+    avgData = sum(data[1:]) / data[0]
+
+    winner = 0
+    for i in range(1, len(data)):
+        if data[i] > avgData:
+            winner += 1
+
+    print('{:.3f}%'.format(winner/data[0]*100))
