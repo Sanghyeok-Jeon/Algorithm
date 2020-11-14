6094a967979bf669dd4f3287fe8e6d04db0211c1
전상혁 <sh_jeon@kakao.com>
2020-11-14 14:54:22 +0900

    2020-11-14 14:54:21

---
+++ Python/BAEKJOON/BOJ_2581_소수.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open('BOJ_2581.txt', 'r')
+
+M = int(input())
+N = int(input())
+
+sosu = [1] * (N+1)
+
+for i in range(N//2+1):
+    if i < 2:
+        sosu[i] = 0
+    else:
+        for j in range(i*2, N+1, i):
+            sosu[j] = 0
+
+first = 0
+sumSosu = 0
+for i in range(M, N+1):
+    if sosu[i] == 1:
+        if first == 0:
+            first = i
+        sumSosu += i
+
+if sumSosu == 0:
+    print(-1)
+else:
+    print(sumSosu)
+    print(first)
+
+
+
