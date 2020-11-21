ade51e560ef11f0fb02430b4c33cbab6d10a0a4b
전상혁 <sh_jeon@kakao.com>
2020-11-21 16:29:18 +0900

    2020-11-21 16:29:18

---
+++ Python/BAEKJOON/BOJ_1964_오각형오각형오각형.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('BOJ_1964.txt', 'r')
+
+N = int(input())
+
+sumDot = 0
+for i in range(1, N+1):
+    if i == 1:
+        sumDot += 5
+    else:
+        sumDot += i*3 + 1
+
+print(sumDot % 45678)
