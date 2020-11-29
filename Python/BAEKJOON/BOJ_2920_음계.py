8944d1698a1c0c11cfd004a88491504be43cf809
전상혁 <sh_jeon@kakao.com>
2020-11-29 21:55:53 +0900

    2020-11-29 21:55:52

---
+++ Python/BAEKJOON/BOJ_2920_음계.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('BOJ_2920.txt', 'r')
+
+data = list(map(int, input().split()))
+asc = [1, 2, 3, 4, 5, 6, 7, 8]
+desc = [8, 7, 6, 5, 4, 3, 2, 1]
+
+if data == asc:
+    print('ascending')
+elif data == desc:
+    print('descending')
+else:
+    print('mixed')
