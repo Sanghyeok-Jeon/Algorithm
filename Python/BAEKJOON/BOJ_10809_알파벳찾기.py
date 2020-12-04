87f392cb800fff62f750af5e47a977b04d27b56a
전상혁 <sh_jeon@kakao.com>
2020-12-04 21:52:06 +0900

    2020-12-04 21:52:06

---
+++ Python/BAEKJOON/BOJ_10809_알파벳찾기.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('BOJ_10809.txt', 'r')
+
+S = input()
+alphabet = [-1] * 26
+for i in range(len(S)):
+    ch = S[i]
+    if alphabet[ord(ch)-ord('a')] == -1:
+        alphabet[ord(ch)-ord('a')] = i
+
+print(*alphabet)
