2c3c970eba75eaabc11bb73b138dcbcb8660f551
전상혁 <sh_jeon@kakao.com>
2020-12-05 19:58:48 +0900

    2020-12-05 19:58:48

---
+++ Python/BAEKJOON/BOJ_2577_숫자의개수.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('BOJ_2577.txt', 'r')
+
+A, B, C = int(input()), int(input()), int(input())
+data = str(A * B * C)
+num = [0] * 10
+for i in range(len(data)):
+    num[int(data[i])] += 1
+
+for i in range(10):
+    print(num[i])
+
