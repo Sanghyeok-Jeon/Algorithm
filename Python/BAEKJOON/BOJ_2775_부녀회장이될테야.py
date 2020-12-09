41c85ed02083791b114e7a5f524cc1588b7e50a8
전상혁 <sh_jeon@kakao.com>
2020-12-09 21:58:19 +0900

    2020-12-09 21:58:18

---
+++ Python/BAEKJOON/BOJ_2775_부녀회장이될테야.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('BOJ_2775.txt', 'r')
+
+T = int(input())
+for tc in range(T):
+    k, n = int(input()), int(input())
+    dp = [[0]*(n+1) for _ in range(k+1)]
+    for i in range(1, n+1):
+        dp[0][i] = i + dp[0][i-1]
+
+    for i in range(1, k):
+        for j in range(1, n+1):
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+    print(dp[k-1][n])
