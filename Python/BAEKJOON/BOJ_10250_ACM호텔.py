2a5b4e69a6dd0bcd49ee6a83fc000d6b32772be3
전상혁 <sh_jeon@kakao.com>
2020-12-11 00:12:22 +0900

    2020-12-11 00:12:22

---
+++ Python/BAEKJOON/BOJ_10250_ACM호텔.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('BOJ_10250.txt', 'r')
+
+T = int(input())
+for tc in range(T):
+    H, W, N = map(int, input().split())
+
+    floor = N%H if N%H else H
+    room = N//H + 1 if N%H else N//H
+
+    print(floor*100 + room)
