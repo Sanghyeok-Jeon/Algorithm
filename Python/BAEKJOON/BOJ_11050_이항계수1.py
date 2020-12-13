ac6e5a81075233e138e25a57dfef4398c8fac682
전상혁 <sh_jeon@kakao.com>
2020-12-13 18:36:00 +0900

    2020-12-13 18:36:00

---
+++ Python/BAEKJOON/BOJ_11050_이항계수1.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('BOJ_11050.txt', 'r')
+
+N, K = map(int, input().split())
+bunmo, bunja = 1, 1
+
+for i in range(max(N-K, K)+1, N+1):
+    bunmo *= i
+
+for i in range(1, min(N-K, K)+1):
+    bunja *= i
+
+print(bunmo//bunja)
