e38a53c55a4ccb17805a03295813d822f66c51ff
전상혁 <sh_jeon@kakao.com>
2020-12-14 21:28:06 +0900

    2020-12-14 21:28:06

---
+++ Python/BAEKJOON/BOJ_1181_단어정렬.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_1181.txt', 'r')
+
+N = int(input())
+data = set()
+for _ in range(N):
+    d = input()
+    data.add((d, len(d)))
+
+dt = sorted(data, key=lambda x: (x[1], x[0]))
+
+for d in dt:
+    print(d[0])
+
