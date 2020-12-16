8377abfe0dac9e341bb39c05dd298ba3700ce50b
전상혁 <sh_jeon@kakao.com>
2020-12-16 21:47:57 +0900

    2020-12-16 21:47:57

---
+++ Python/BAEKJOON/BOJ_4949_균형잡힌세상.py
@@ -0,0 +1,33 @@
+import sys
+sys.stdin = open('BOJ_4949.txt', 'r')
+
+while True:
+    data = input()
+
+    if data == '.':
+        break
+
+    yesPossible = 1
+    stack = []
+    for d in data:
+        if d == '(' or d == '[':
+            stack.append(d)
+        elif d == ')':
+            if len(stack) and stack[-1] == '(':
+                stack.pop()
+            else:
+                yesPossible = 0
+                break
+        elif d == ']':
+            if len(stack) and stack[-1] == '[':
+                stack.pop()
+            else:
+                yesPossible = 0
+                break
+        else:
+            continue
+
+    if yesPossible and not len(stack):
+        print('yes')
+    else:
+        print('no')
