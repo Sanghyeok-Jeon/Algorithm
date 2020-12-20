c0f9f18cd6920e1b5aa47f07c1902b6e9443f7af
전상혁 <sh_jeon@kakao.com>
2020-12-20 15:13:07 +0900

    2020-12-20 15:13:06

---
+++ Python/BAEKJOON/BOJ_7568_덩치.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('BOJ_7568.txt', 'r')
+
+N = int(input())
+profile = []
+for _ in range(N):
+    weight, height = map(int, input().split())
+    profile.append([weight, height])
+
+for p1 in profile:
+    cnt = 1
+    for p2 in profile:
+        if p1[0] < p2[0] and p1[1] < p2[1]:
+            cnt += 1
+    print(cnt, end=' ')
