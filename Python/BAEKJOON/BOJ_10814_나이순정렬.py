b254df49b8960e8d0c2538dfe89341e42159369a
전상혁 <sh_jeon@kakao.com>
2020-12-22 20:16:47 +0900

    2020-12-22 20:16:47

---
+++ Python/BAEKJOON/BOJ_10814_나이순정렬.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_10814.txt', 'r')
+
+N = int(sys.stdin.readline())
+member = []
+idx = 0
+for m in sys.stdin:
+    member.append((m.split(), idx))
+    idx += 1
+
+member.sort(key=lambda x : (int(x[0][0]), x[1]))
+
+for m in member:
+    print(m[0][0], m[0][1])
