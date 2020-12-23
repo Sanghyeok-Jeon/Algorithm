40ec73d67e27fc55e98ecbeff7cff56a3c8d6547
전상혁 <sh_jeon@kakao.com>
2020-12-23 21:04:57 +0900

    2020-12-23 21:04:57

---
+++ Python/BAEKJOON/BOJ_10989_수정렬하기3.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('BOJ_10989.txt', 'r')
+
+N = int(sys.stdin.readline())
+cntNum = [0] * 10001
+for n in sys.stdin:
+    cntNum[int(n)] += 1
+
+cnt = 0
+for i in range(1, 10001):
+    if cntNum[i]:
+        for _ in range(cntNum[i]):
+            print(i)
+            cnt += 1
+
+    if cnt == N:
+        break
