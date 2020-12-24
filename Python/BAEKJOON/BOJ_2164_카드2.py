f5b09363a9b38d503f1b119588f7d74a0697f0fc
전상혁 <sh_jeon@kakao.com>
2020-12-24 19:30:56 +0900

    2020-12-24 19:30:56

---
+++ Python/BAEKJOON/BOJ_2164_카드2.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('BOJ_2164.txt', 'r')
+
+import collections
+
+N = int(input())
+q = collections.deque()
+for i in range(1, N+1):
+    q.append(i)
+
+cnt = 0
+while len(q) > 1:
+    if cnt % 2:
+        q.append(q.popleft())
+    else:
+        q.popleft()
+    cnt += 1
+
+print(q[0])
