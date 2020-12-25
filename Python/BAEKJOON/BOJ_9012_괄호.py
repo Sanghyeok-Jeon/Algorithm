07a39ad3d0400c7f4c3d5cdfa9c2861833bf87af
전상혁 <sh_jeon@kakao.com>
2020-12-25 12:09:52 +0900

    2020-12-25 12:09:52

---
+++ Python/BAEKJOON/BOJ_9012_괄호.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('BOJ_9012.txt')
+
+T = int(input())
+for _ in range(T):
+    ps = input()
+    stack = []
+
+    impossible = 0
+    for p in ps:
+        if p == "(":
+            stack.append(p)
+        else:
+            if len(stack) and stack[-1] == "(":
+                stack.pop()
+            else:
+                impossible = 1
+                break
+
+    if impossible == 0 and len(stack) == 0:
+        print('YES')
+    else:
+        print('NO')
