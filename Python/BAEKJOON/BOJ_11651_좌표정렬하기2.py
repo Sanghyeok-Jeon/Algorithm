245decc74d8ccb13791bd64c56c67279220846d3
전상혁 <sh_jeon@kakao.com>
2020-12-29 21:39:36 +0900

    2020-12-29 21:39:36

---
+++ Python/BAEKJOON/BOJ_11651_좌표정렬하기2.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('BOJ_11651.txt', 'r')
+
+N = int(sys.stdin.readline())
+coordinate = []
+for _ in range(N):
+    x, y = map(int, sys.stdin.readline().split())
+    coordinate.append((x, y))
+
+coordinate.sort(key=lambda x:(x[1], x[0]))
+
+for xy in coordinate:
+    print('{} {}'.format(xy[0], xy[1]))
