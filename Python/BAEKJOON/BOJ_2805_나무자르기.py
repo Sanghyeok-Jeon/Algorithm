fdbce10ce96a23eb4d48324deb48a47e36fa3f83
전상혁 <sh_jeon@kakao.com>
2021-01-05 21:42:23 +0900

    2021-01-05 21:42:22

---
+++ Python/BAEKJOON/BOJ_2805_나무자르기.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('BOJ_2805.txt', 'r')
+
+N, M = map(int, sys.stdin.readline().split())
+woods = list(map(int, sys.stdin.readline().split()))
+woods.sort()
+
+short, long = 1, woods[-1]
+while short <= long:
+    mid = (short + long) // 2
+
+    temp = 0
+    for wood in woods:
+        if wood > mid:
+            temp += wood - mid
+    if temp >= M:
+        short = mid + 1
+    else:
+        long = mid - 1
+
+print(long)
