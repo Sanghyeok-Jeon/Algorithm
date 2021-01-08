71ef1a3ea5c31fb38c194bcb7926cc35f921b233
전상혁 <sh_jeon@kakao.com>
2021-01-08 21:52:25 +0900

    2021-01-08 21:52:25

---
+++ Python/BAEKJOON/BOJ_2108_통계학.py
@@ -0,0 +1,20 @@
+import sys
+import collections
+sys.stdin = open('BOJ_2108.txt', 'r')
+
+N = int(sys.stdin.readline())
+data = sorted([int(sys.stdin.readline()) for _ in range(N)])
+
+print(round(sum(data)/N))
+print(data[N//2])
+
+cntData = collections.Counter(data).most_common()
+if len(cntData) > 1:
+    if cntData[0][1] == cntData[1][1]:
+        print(cntData[1][0])
+    else:
+        print(cntData[0][0])
+else:
+    print(cntData[0][0])
+
+print(data[-1] - data[0])
