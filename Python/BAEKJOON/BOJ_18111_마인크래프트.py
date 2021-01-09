35031104644baeea27ffe0360bc526fc5b9143d7
전상혁 <sh_jeon@kakao.com>
2021-01-09 15:34:05 +0900

    2021-01-09 15:34:04

---
+++ Python/BAEKJOON/BOJ_18111_마인크래프트.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin = open('BOJ_18111.txt', 'r')
+
+N, M, B = map(int, sys.stdin.readline().split())
+field = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+time = 256 * N * M + 1
+height = 0
+MAX, MIN = max(map(max, field)), min(map(min, field))
+
+for f in range(MIN, MAX+1):
+    fieldUp, fieldDown = 0, 0
+    for j in range(N):
+        for k in range(M):
+            if field[j][k] < f:
+                fieldUp += f - field[j][k]
+            else:
+                fieldDown += field[j][k] - f
+    inventory = fieldDown + B
+    if inventory >= fieldUp:
+        tempTime = 2 * fieldDown + fieldUp
+        if tempTime <= time:
+            time = tempTime
+            height = f
+
+print(time, height)
