f7803729e7d19b48eec1ec3ab89a024c95b99400
전상혁 <sh_jeon@kakao.com>
2021-01-11 21:24:12 +0900

    2021-01-11 21:24:11

---
+++ Python/BAEKJOON/BOJ_17626_FourSquares.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('BOJ_17626.txt', 'r')
+
+n = int(sys.stdin.readline())
+answer = 4
+for i in range(int(n**0.5), int((n//4)**0.5), -1):
+    if n == i*i:
+        answer = 1
+        break
+    else:
+        m = n - i*i
+        for j in range(int(m**0.5), int((m//3)**0.5), -1):
+            if n == i*i + j*j:
+                answer = min(answer, 2)
+            else:
+                l = n - i*i - j*j
+                for k in range(int(l**0.5), int((l//2)**0.5), -1):
+                    if n == i*i + j*j + k*k:
+                        answer = min(answer, 3)
+
+print(answer)
