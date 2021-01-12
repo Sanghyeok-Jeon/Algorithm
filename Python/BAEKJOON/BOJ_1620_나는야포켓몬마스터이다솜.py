1425b5837ad9b121e002bd8838dea9e98d3ec60f
전상혁 <sh_jeon@kakao.com>
2021-01-12 21:16:10 +0900

    2021-01-12 21:16:10

---
+++ Python/BAEKJOON/BOJ_1620_나는야포켓몬마스터이다솜.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('BOJ_1620.txt', 'r')
+
+N, M = map(int, sys.stdin.readline().split())
+dicPokeNo = {}
+dicPokeNm = {}
+
+pokeNo = 1
+for _ in range(N):
+    pokeNm = sys.stdin.readline().rstrip()
+    dicPokeNm[pokeNm] = pokeNo
+    dicPokeNo[pokeNo] = pokeNm
+    pokeNo += 1
+
+for _ in range(M):
+    problem = sys.stdin.readline().rstrip()
+
+    try:
+        print(dicPokeNo[int(problem)])
+    except:
+        print(dicPokeNm[problem])
