81b5ff10b6de4f23131a10167e6ae422f287d54f
전상혁 <sh_jeon@kakao.com>
2021-01-13 21:37:32 +0900

    2021-01-13 21:37:32

---
+++ Python/BAEKJOON/BOJ_1764_듣보잡.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('BOJ_1764.txt', 'r')
+
+N, M = map(int, sys.stdin.readline().split())
+setN = set()
+setM = set()
+
+for _ in range(N):
+    setN.add(sys.stdin.readline().rstrip())
+for _ in range(M):
+    setM.add(sys.stdin.readline().rstrip())
+
+answer = sorted(list(setN & setM))
+print(len(answer))
+for ans in answer:
+    print(ans)
