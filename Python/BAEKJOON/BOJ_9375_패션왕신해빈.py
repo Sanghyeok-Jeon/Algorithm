20aee94d57f41eefdf0c176f684bf24887240a31
전상혁 <sh_jeon@kakao.com>
2021-01-17 02:09:47 +0900

    2021-01-17 02:09:46

---
+++ Python/BAEKJOON/BOJ_9375_패션왕신해빈.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('BOJ_9375.txt', 'r')
+
+T = int(sys.stdin.readline())
+for _ in range(T):
+    n = int(sys.stdin.readline())
+    dicWear = {}
+
+    for _ in range(n):
+        wear, wearKind = sys.stdin.readline().split()
+
+        if wearKind in dicWear.keys():
+            dicWear[wearKind] += 1
+        else:
+            dicWear[wearKind] = 1
+
+    ans = 1
+    for w in dicWear.values():
+        ans *= w + 1
+
+    print(ans - 1)
