08fb196d1d0a32064f6c30b5d32e4ebf8b4908bb
전상혁 <sh_jeon@kakao.com>
2021-01-30 10:44:58 +0900

    2021-01-30 10:44:58

---
+++ Python/BAEKJOON/BOJ_6064_카잉달력.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_6064.txt', 'r')
+
+def calendarCain(M, N, x, y):
+    while x <= M * N:
+        if not (x - y) % N:
+            return x
+        x += M
+    return -1
+
+T = int(sys.stdin.readline())
+for _ in range(T):
+    M, N, x, y = map(int, sys.stdin.readline().rstrip().split())
+    print(calendarCain(M, N, x, y))
