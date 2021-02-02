265d51dc77fe5727fd27386de7884ea241eb239f
전상혁 <sh_jeon@kakao.com>
2021-02-02 21:05:28 +0900

    2021-02-02 21:05:28

---
+++ Python/BAEKJOON/BOJ_1107_리모컨.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open('BOJ_1107.txt', 'r')
+
+def checkNum(tmpNum):
+    tmpNum = str(tmpNum)
+    for n in tmpNum:
+        if n in btnBroken:
+            return False
+    return True
+
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+btnBroken = list(sys.stdin.readline().rstrip().split())
+cntPush = abs(N - 100)
+
+for tmpNum in range(1000001):
+    if checkNum(tmpNum):
+        cntPush = min(cntPush, len(str(tmpNum)) + abs(N - tmpNum))
+
+print(cntPush)
