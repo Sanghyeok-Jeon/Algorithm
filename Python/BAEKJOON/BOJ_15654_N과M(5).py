c156f32595232013b90326187bba436883224639
전상혁 <sh_jeon@kakao.com>
2021-02-25 21:26:06 +0900

    2021-02-25 21:26:06

---
+++ Python/BAEKJOON/BOJ_15654_N과M(5).py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('BOJ_15654.txt', 'r')
+
+def DFS(cnt):
+    if cnt == M:
+        print(*result)
+        return
+
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = 1
+            result.append(lstNo[i])
+            DFS(cnt+1)
+            result.pop()
+            visited[i] = 0
+
+N, M = map(int, input().split())
+lstNo = list(map(int, input().split()))
+lstNo.sort()
+visited = [0] * N
+result = []
+
+DFS(0)
