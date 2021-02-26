425e0a99deb5867a3fab16efd145698006138ba9
전상혁 <sh_jeon@kakao.com>
2021-02-26 19:05:38 +0900

    2021-02-26 19:05:38

---
+++ JavaScript/서울에서김서방찾기.js
@@ -0,0 +1,14 @@
+function solution(seoul) {
+    var answer = '';
+    var idx = 0;
+    for (var i=0; i < seoul.length ; i++) {
+        if (seoul[i] == "Kim"){
+            idx = i;
+        }
+    }
+    
+    if () {
+        answer = "김서방은 " + idx + "에 있다";
+    }
+    return answer;
+}
