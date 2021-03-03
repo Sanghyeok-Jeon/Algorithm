f59b69418a4f3ab47142f1ec6c3ab94b31d1b0e3
전상혁 <sh_jeon@kakao.com>
2021-03-03 21:22:47 +0900

    2021-03-03 21:22:47

---
+++ Python/BAEKJOON/BOJ_2407_조합.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_2407.txt', 'r')
+
+n, m = map(int, input().split())
+
+answer = 1
+for i in range(1, n+1):
+    answer *= i
+for i in range(1, n-m+1):
+    answer //=i
+for i in range(1, m+1):
+    answer //=i
+
+print(answer)
