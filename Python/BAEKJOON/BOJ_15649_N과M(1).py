44f3a90561c0a84addc3a32c669c81021723d545
전상혁 <sh_jeon@kakao.com>
2021-03-16 00:09:40 +0900

    2021-03-16 00:09:40

---
+++ Python/BAEKJOON/BOJ_15649_N과M(1).py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('BOJ_15649.txt', 'r')
+
+def DFS(cnt):
+    if cnt == M:
+        print(*ans)
+        return
+
+    for i in range(1, N+1):
+        if i not in ans:
+            ans.append(i)
+            DFS(cnt+1)
+            ans.pop()
+
+N, M = map(int, input().split())
+
+ans = []
+DFS(0)
