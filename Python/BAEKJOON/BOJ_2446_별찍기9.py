0766c9e5d134a0e8f59ddc153b2e60b21330efaf
전상혁 <sh_jeon@kakao.com>
2021-03-27 19:18:21 +0900

    2021-03-27 19:18:21

---
+++ Python/BAEKJOON/BOJ_2446_별찍기9.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open('BOJ_2446.txt', 'r')
+
+N = int(input())
+for n in range(2*N+1):
+    if n == N or n == N+1:
+        continue
+    print(' '*(N-abs(N-n)) + '*'*(2*abs(N-n)-1))
