2e69848fbb97f495020fb87fdb1f5c18bc5819a4
전상혁 <sh_jeon@kakao.com>
2021-04-06 00:00:06 +0900

    2021-04-06 00:00:06

---
+++ Python/BAEKJOON/BOJ_3009_네번째점.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('BOJ_3009.txt', 'r')
+
+X = []
+Y = []
+for _ in range(3):
+    x, y = map(int, input().split())
+    if x in X:
+        X.remove(x)
+    else:
+        X.append(x)
+
+    if y in Y:
+        Y.remove(y)
+    else:
+        Y.append(y)
+
+print('{} {}'.format(X[0], Y[0]))
