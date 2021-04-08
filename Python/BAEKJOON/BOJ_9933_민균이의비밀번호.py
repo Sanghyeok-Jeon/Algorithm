eef96baccb2e2422b2a7246d39dc05b53ee81199
전상혁 <sh_jeon@kakao.com>
2021-04-08 22:12:44 +0900

    2021-04-08 22:12:44

---
+++ Python/BAEKJOON/BOJ_9933_민균이의비밀번호.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open('BOJ_9933.txt', 'r')
+
+N = int(input())
+data = [input() for _ in range(N)]
+for n in range(N):
+    if data[n][::-1] in data:
+        lenStr = len(data[n])
+        print(lenStr, data[n][lenStr//2])
+        break
