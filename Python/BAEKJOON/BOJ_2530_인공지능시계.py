ec93ad6c70636bc5561a35e20514134fcf34c1a0
전상혁 <sh_jeon@kakao.com>
2021-04-25 21:50:51 +0900

    2021-04-25 21:50:50

---
+++ Python/BAEKJOON/BOJ_2530_인공지능시계.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('BOJ_2530.txt', 'r')
+
+A, B, C = map(int, input().split())
+D = int(input())
+D += A*3600 + B*60 + C
+
+newA = D//3600%24
+newB = (D%3600)//60
+newC = D%60
+
+print('{} {} {}'.format(newA, newB, newC))
