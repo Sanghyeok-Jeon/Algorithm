4292c80168c7e40828dcce38143f7073444bcea8
전상혁 <sh_jeon@kakao.com>
2021-04-27 21:31:27 +0900

    2021-04-27 21:31:27

---
+++ Python/BAEKJOON/BOJ_2954_창영이의일기장.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('BOJ_2954.txt', 'r')
+
+S = input()
+N = len(S)
+answer = ''
+
+i = 0
+while i < len(S):
+    if S[i] in ['a', 'e', 'i', 'o', 'u']:
+        answer += S[i]
+        i += 3
+    else:
+        answer += S[i]
+        i += 1
+
+print(answer)
