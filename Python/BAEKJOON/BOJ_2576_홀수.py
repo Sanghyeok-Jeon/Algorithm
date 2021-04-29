c5b565d60200f95fa627eda30d7f9e4224af4524
전상혁 <sh_jeon@kakao.com>
2021-04-29 21:52:43 +0900

    2021-04-29 21:52:43

---
+++ Python/BAEKJOON/BOJ_2576_홀수.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('BOJ_2576.txt', 'r')
+
+listNum = [int(input()) for _ in range(7)]
+listNum.sort()
+
+sumOdd = 0
+minOdd = -1
+for n in listNum:
+    if n % 2:
+        if minOdd == -1:
+            minOdd = n
+        sumOdd += n
+
+if minOdd == -1:
+    print(minOdd)
+else:
+    print(sumOdd)
+    print(minOdd)
