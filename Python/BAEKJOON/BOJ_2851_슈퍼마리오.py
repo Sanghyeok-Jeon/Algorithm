a2eb9efce7ffbbac497b406f456e44c0c5b4e154
전상혁 <sh_jeon@kakao.com>
2021-05-03 20:44:59 +0900

    2021-05-03 20:44:59

---
+++ Python/BAEKJOON/BOJ_2851_슈퍼마리오.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_2851.txt', 'r')
+
+mushrooms = [int(input()) for _ in range(10)]
+
+score = 0
+for mr in mushrooms:
+    score += mr
+    if score >= 100:
+        if score - 100 > 100 - (score - mr):
+            score -= mr
+        break
+
+print(score)
