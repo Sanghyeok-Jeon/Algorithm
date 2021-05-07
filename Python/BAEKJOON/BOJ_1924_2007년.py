9c454833f7c7aa647829cee4c464fef32a724e8f
전상혁 <sh_jeon@kakao.com>
2021-05-07 23:55:03 +0900

    2021-05-07 23:55:03

---
+++ Python/BAEKJOON/BOJ_1924_2007년.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open('BOJ_1924.txt', 'r')
+
+month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+weekdays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+x, y = map(int, input().split())
+days = sum(month[:x-1]) + y-1
+print(weekdays[days%7])
