2e31fdbc0a99863a2535145243b2c3a9aeba6883
전상혁 <sh_jeon@kakao.com>
2021-05-09 17:12:22 +0900

    2021-05-09 17:12:22

---
+++ Python/BAEKJOON/BOJ_2846_오르막길.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('BOJ_2846.txt', 'r')
+
+N = int(input())
+load = list(map(int, input().split()))
+
+maxUphill = 0
+tmpUphill = 0
+for i in range(1, N):
+    if load[i] > load[i-1]:
+        tmpUphill += load[i] - load[i-1]
+    else:
+        tmpUphill = 0
+    maxUphill = max(maxUphill, tmpUphill)
+
+print(maxUphill)
