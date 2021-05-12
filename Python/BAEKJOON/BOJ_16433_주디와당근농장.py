3caa263003ed8df4922a24f97ad17a76f8e84f3b
전상혁 <sh_jeon@kakao.com>
2021-05-12 21:19:36 +0900

    2021-05-12 21:19:36

---
+++ Python/BAEKJOON/BOJ_16433_주디와당근농장.py
@@ -0,0 +1,23 @@
+import sys
+sys.setrecursionlimit(10000000)
+sys.stdin = open('BOJ_16433.txt', 'r')
+
+def Carrot(i, j):
+    farm[i][j] = 'v'
+
+    for mode in range(4):
+        ni = i + di[mode]
+        nj = j + dj[mode]
+        if 0 <= ni < N and 0 <= nj < N and farm[ni][nj] == '.':
+            Carrot(ni, nj)
+
+N, R, C = map(int, input().split())
+farm = [['.']*N for _ in range(N)]
+r, c = R-1, C-1
+
+di = [-1, -1, 1, 1]
+dj = [-1, 1, 1, -1]
+Carrot(r, c)
+
+for i in range(N):
+    print(''.join(farm[i]))
