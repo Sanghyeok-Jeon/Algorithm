3e0c4ee1d9e6fe560a62917605b111bdc8f6c739
전상혁 <sh_jeon@kakao.com>
2021-05-13 20:26:40 +0900

    2021-05-13 20:26:39

---
+++ Python/BAEKJOON/BOJ_11091_알파벳전부쓰기.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open('BOJ_11091.txt', 'r')
+
+N = int(input())
+for _ in range(N):
+    data = input().lower()
+    alpha = [0] * 26
+
+    for d in data:
+        if d.isalpha():
+            alpha[ord(d)-ord('a')] += 1
+
+    if 0 in alpha:
+        print('missing', end=' ')
+        for i, v in enumerate(alpha):
+            if not v:
+                print(chr(ord('a') + i), end='')
+        print()
+    else:
+        print('pangram')
