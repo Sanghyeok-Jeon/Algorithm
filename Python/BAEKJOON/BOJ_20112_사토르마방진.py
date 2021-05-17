9b039f4cba100b4d4902884f9f3edf8d16cda72f
전상혁 <sh_jeon@kakao.com>
2021-05-17 19:55:19 +0900

    2021-05-17 19:55:18

---
+++ Python/BAEKJOON/BOJ_20112_사토르마방진.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_20112.txt', 'r')
+
+N = int(input())
+data = [list(input()) for _ in range(N)]
+data2 = list(zip(*data))
+
+checkPossible = True
+for i in range(N):
+    if data[i] != list(data2[i]):
+        checkPossible = False
+        break
+
+print('YES' if checkPossible else 'NO')
