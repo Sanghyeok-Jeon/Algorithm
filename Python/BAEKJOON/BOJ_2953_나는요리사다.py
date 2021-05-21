84fc1d8571707d76b3519a602163a3b5f883e015
전상혁 <sh_jeon@kakao.com>
2021-05-21 21:28:16 +0900

    2021-05-21 21:28:16

---
+++ Python/BAEKJOON/BOJ_2953_나는요리사다.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_2953.txt', 'r')
+
+winner = -1
+score = 0
+for i in range(1, 6):
+    tmpWinner = i
+    tmpScore = sum(map(int, input().split()))
+
+    if tmpScore > score:
+        score = tmpScore
+        winner = tmpWinner
+
+print(winner, score)
