bc2fb27cc58276c0ebfb19e25e687460609fc7a0
전상혁 <sh_jeon@kakao.com>
2021-05-24 20:42:01 +0900

    2021-05-24 20:42:01

---
+++ Python/BAEKJOON/BOJ_10162_전자레인지.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('BOJ_10162.txt', 'r')
+
+T = int(input())
+
+calA = T // 300
+A = calA if calA else 0
+T %= 300
+
+calB = T // 60
+B = calB if calB else 0
+T %= 60
+
+calC = T // 10
+C = calC if calC else 0
+T %= 10
+
+if T:
+    print(-1)
+else:
+    print(A, B, C)
