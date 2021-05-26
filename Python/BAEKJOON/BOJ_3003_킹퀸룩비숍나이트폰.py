b10af608f60d9355305e4bb8aadb9b4f4e8acbe9
전상혁 <sh_jeon@kakao.com>
2021-05-26 20:27:13 +0900

    2021-05-26 20:27:13

---
+++ Python/BAEKJOON/BOJ_3003_킹퀸룩비숍나이트폰.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('BOJ_3003.txt', 'r')
+
+nowKing, nowQueen, nowRook, nowBishop, nowKnight, nowPawn = map(int, input().split())
+correctKing, correctQueen, correctRook, correctBishop, correctKnight, correctPawn = 1, 1, 2, 2, 2, 8
+
+print(correctKing - nowKing,
+      correctQueen - nowQueen,
+      correctRook - nowRook,
+      correctBishop - nowBishop,
+      correctKnight - nowKnight,
+      correctPawn - nowPawn)
