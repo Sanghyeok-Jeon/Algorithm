dc991ea8dc178d266e2a32ec325310b2b9a6a4e1
전상혁 <sh_jeon@kakao.com>
2021-05-30 22:14:47 +0900

    2021-05-30 22:14:47

---
+++ Python/BAEKJOON/BOJ_11586_지영공주님의마법거울.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('BOJ_11586.txt', 'r')
+
+N = int(input())
+mirror = [input() for _ in range(N)]
+K = int(input())
+
+if K == 1:
+    print('\n'.join(mirror))
+elif K == 2:
+    for i in range(N):
+        print(mirror[i][::-1])
+else:
+    for i in range(N-1, -1, -1):
+        print(mirror[i])
