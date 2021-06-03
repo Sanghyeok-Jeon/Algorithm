4f87d74322fe1d189d116b3db9bd638c9d905e79
전상혁 <sh_jeon@kakao.com>
2021-06-03 19:38:30 +0900

    2021-06-03 19:38:30

---
+++ Python/BAEKJOON/BOJ_11721_열개씩끊어출력하기.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('BOJ_11721.txt', 'r')
+
+data = input()
+
+cnt = 1
+for i in range(len(data)):
+    if cnt < 10:
+        print(data[i], end='')
+        cnt += 1
+    elif cnt == 10:
+        print(data[i])
+        cnt = 1
