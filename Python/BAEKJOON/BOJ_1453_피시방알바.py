69c5d351d3df7d539435d7534b3f30d09a4c8ba2
전상혁 <sh_jeon@kakao.com>
2021-06-21 21:52:51 +0900

    2021-06-21 21:52:51

---
+++ Python/BAEKJOON/BOJ_1453_피시방알바.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('BOJ_1453.txt', 'r')
+
+N = int(input())
+guest = list(map(int, input().split()))
+pcNo = [0] * 101
+
+rejectGuest = 0
+for g in guest:
+    if pcNo[g]:
+        rejectGuest += 1
+    else:
+        pcNo[g] = 1
+
+print(rejectGuest)
