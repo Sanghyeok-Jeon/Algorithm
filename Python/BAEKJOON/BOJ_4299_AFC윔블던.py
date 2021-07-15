30b678d1eccb42ec77ddc15bf57b5bad18c07dcb
전상혁 <sh_jeon@kakao.com>
2021-07-16 00:01:14 +0900

    2021-07-16 00:01:13

---
+++ Python/BAEKJOON/BOJ_4299_AFC윔블던.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_4299.txt', 'r')
+
+S, D = map(int, input().split())
+
+if not (S+D)%2 and not (S-D)%2:
+    A = (S + D) // 2
+    B = (S - D) // 2
+    if A >= 0 and B >= 0:
+        print('{} {}'.format(A, B) if A > B else '{} {}'.format(B, A))
+    else:
+        print(-1)
+else:
+    print(-1)
