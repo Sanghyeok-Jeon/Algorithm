101612ba2d5af9132129adc560d987a4d03f7f48
전상혁 <sh_jeon@kakao.com>
2021-07-19 22:37:26 +0900

    2021-07-19 22:37:26

---
+++ Python/BAEKJOON/BOJ_15873_공백없는A+B.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_15873.txt', 'r')
+
+AB = input()
+
+if len(AB) == 2:
+    print(int(AB[0]) + int(AB[1]))
+elif len(AB) == 3:
+    if AB[1] == '0':
+        print(int(AB[:2]) + int(AB[2]))
+    else:
+        print(int(AB[0]) + int(AB[1:]))
+else:
+    print(20)
