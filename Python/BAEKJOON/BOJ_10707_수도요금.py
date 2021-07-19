de7fa21d02672721b3cc6d68e5622a826236cbc0
전상혁 <sh_jeon@kakao.com>
2021-07-19 21:06:38 +0900

    2021-07-19 21:06:38

---
+++ Python/BAEKJOON/BOJ_10707_수도요금.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('BOJ_10707.txt', 'r')
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+P = int(input())
+
+X = A * P
+Y = B + D * (P-C) if P > C else B
+
+print(X if X < Y else Y)
