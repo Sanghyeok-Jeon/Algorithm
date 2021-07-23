cf0ef7f83fb9575a5f3efde5ef58dcb8d3872b96
전상혁 <sh_jeon@kakao.com>
2021-07-24 00:42:41 +0900

    2021-07-24 00:42:41

---
+++ Python/BAEKJOON/BOJ_16431_베시와데이지.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('BOJ_16431.txt', 'r')
+
+Br, Bc = map(int, input().split())
+Dr, Dc = map(int, input().split())
+Jr, Jc = map(int, input().split())
+
+moveB = max(abs(Br-Jr), abs(Bc-Jc))
+moveD = abs(Dr-Jr) + abs(Dc-Jc)
+
+print('bessie' if moveB < moveD else 'tie' if moveB == moveD else 'daisy')
