1e1422072db77d22c33bc78f93de28d2b52e2798
전상혁 <sh_jeon@kakao.com>
2021-07-25 16:22:15 +0900

    2021-07-25 16:22:15

---
+++ Python/BAEKJOON/BOJ_17388_와글와글숭고한.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('BOJ_17388.txt', 'r')
+
+S, K, H = map(int, input().split())
+
+if S + K + H >= 100:
+    print('OK')
+else:
+    minScore = min(S, K, H)
+    if minScore == S:
+        print('Soongsil')
+    elif minScore == K:
+        print('Korea')
+    else:
+        print('Hanyang')
