9a1efea726abacf3eb63cb64573feafcc50b05a1
전상혁 <sh_jeon@kakao.com>
2021-08-05 20:57:13 +0900

    2021-08-05 20:57:13

---
+++ Python/BAEKJOON/BOJ_2506_점수계산.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('BOJ_2506.txt', 'r')
+
+N = int(input())
+nowScore = 0
+totalScore = 0
+for s in map(int, input().split()):
+    if s:
+        if nowScore:
+            nowScore += 1
+        else:
+            nowScore = 1
+        totalScore += nowScore
+    else:
+        nowScore = 0
+print(totalScore)
+
