697abb63b57ba4d6f61f3bd9bbdf6a22a5d1ef1a
전상혁 <sh_jeon@kakao.com>
2021-08-10 09:18:21 +0900

    2021-08-10 09:18:21

---
+++ Python/BAEKJOON/BOJ_3058_짝수를찾아라.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_3058.txt', 'r')
+
+T = int(input())
+for _ in range(T):
+    data = sorted(list(map(int, input().split())))
+    minEven = 0
+    sumEven = 0
+    for d in data:
+        if not d%2:
+            if not minEven:
+                minEven += d
+            sumEven += d
+    print(sumEven, minEven)
