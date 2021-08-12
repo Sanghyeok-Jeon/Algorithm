9c13406d030a3d576325e4bc0668633c9e45475b
전상혁 <sh_jeon@kakao.com>
2021-08-12 22:13:46 +0900

    2021-08-12 22:13:46

---
+++ Python/BAEKJOON/BOJ_2991_사나운개.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('BOJ_2991.txt', 'r')
+
+A, B, C, D = map(int, input().split())
+PMN = list(map(int, input().split()))
+
+for pmn in PMN:
+    attacked = 0
+    if 0 < pmn % (A+B) <= A:
+        attacked += 1
+    if 0 < pmn % (C+D) <= C:
+        attacked += 1
+    print(attacked)
