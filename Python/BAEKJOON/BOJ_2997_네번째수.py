829cb8b7b09e2b63fd748dd843e965185b5bd02d
전상혁 <sh_jeon@kakao.com>
2021-08-20 18:42:23 +0900

    2021-08-20 18:42:23

---
+++ Python/BAEKJOON/BOJ_2997_네번째수.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open('BOJ_2997.txt', 'r')
+
+data = sorted(list(map(int, input().split())))
+if data[1]-data[0] == data[2]-data[1]:
+    print(data[2]+data[1]-data[0])
+elif data[1]-data[0] < data[2]-data[1]:
+    print(data[1]*2-data[0])
+else:
+    print(data[2]-data[1]+data[0])
