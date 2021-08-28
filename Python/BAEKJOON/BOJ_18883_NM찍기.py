fc81226d04f5d693b6bbb11049881007558f50e2
전상혁 <sh_jeon@kakao.com>
2021-08-29 00:37:55 +0900

    2021-08-29 00:37:55

---
+++ Python/BAEKJOON/BOJ_18883_NM찍기.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open('BOJ_18883.txt', 'r')
+
+N, M = map(int, input().split())
+for n in range(N):
+    for m in range(1, M+1):
+        if m != M:
+            print(n*M + m, end=' ')
+        else:
+            print(n*M + m)
