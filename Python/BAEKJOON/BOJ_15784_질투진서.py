05bf327fec0fc06db94cdf31f95faaeca589d1bd
전상혁 <sh_jeon@kakao.com>
2021-09-04 21:44:24 +0900

    2021-09-04 21:44:24

---
+++ Python/BAEKJOON/BOJ_15784_질투진서.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('BOJ_15784.txt', 'r')
+
+N, a, b = map(int, input().split())
+X = [list(map(int, input().split())) for _ in range(N)]
+target = X[a-1][b-1]
+
+handsome = 1
+if max(X[a-1]) != target:
+    handsome = 0
+for i in range(N):
+    if X[i][b-1] > target:
+        handsome = 0
+        break
+
+print('ANGRY' if not handsome else 'HAPPY')
