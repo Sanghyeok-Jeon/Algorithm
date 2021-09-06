3da396ba63755cab58fd13ff5f28f2c4dc4191be
전상혁 <sh_jeon@kakao.com>
2021-09-06 21:14:09 +0900

    2021-09-06 21:14:09

---
+++ Python/BAEKJOON/BOJ_21866_추첨을통해커피를받자.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('BOJ_21866.txt', 'r')
+
+score = list(map(int, input().split()))
+maxScore = [100, 100, 200, 200, 300, 300, 400, 400, 500]
+total = 0
+result = 'none'
+for i in range(9):
+    if score[i] > maxScore[i]:
+        result = 'hacker'
+        break
+    else:
+        total += score[i]
+
+if total >= 100:
+    result = 'draw'
+
+print(result)
