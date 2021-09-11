e3e1b78689f530cd8603cb44cba33b8c43b91e89
전상혁 <sh_jeon@kakao.com>
2021-09-11 21:29:12 +0900

    2021-09-11 21:29:11

---
+++ Python/BAEKJOON/BOJ_15780_멀티탭충분하니.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('BOJ_15780.txt', 'r')
+
+N, K = map(int, input().split())
+maxPossible = 0
+for mt in map(int, input().split()):
+    maxPossible += mt//2 + 1 if mt % 2 else mt//2
+
+print('YES' if N <= maxPossible else 'NO')
