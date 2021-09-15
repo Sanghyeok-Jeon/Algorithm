2bc66952d655767b57354a983c76afefd50da1b2
전상혁 <sh_jeon@kakao.com>
2021-09-16 08:26:37 +0900

    2021-09-16 08:26:36

---
+++ Python/BAEKJOON/BOJ_1703_생장점.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('BOJ_1703.txt', 'r')
+
+while True:
+    data = list(map(int, input().split()))
+    a = data[0]
+    if a == 0:
+        break
+
+    result = 1
+    for i in range(1, 2*a, 2):
+        splittingFactor = data[i]
+        branching = data[i+1]
+
+        result *= splittingFactor
+        result -= branching
+
+    print(result)
