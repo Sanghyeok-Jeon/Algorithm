7eaa018ffcccae73298ca1661a86d4184ebf6e49
전상혁 <sh_jeon@kakao.com>
2021-09-17 00:06:05 +0900

    2021-09-17 00:06:05

---
+++ Python/BAEKJOON/BOJ_2566_최댓값.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('BOJ_2566.txt', 'r')
+
+data = [list(map(int, input().split())) for _ in range(9)]
+
+r = c = 0
+maxData = 0
+for i in range(9):
+    for j in range(9):
+        if data[i][j] >= maxData:
+            maxData = data[i][j]
+            r, c = i+1, j+1
+
+print(maxData)
+print(r, c)
