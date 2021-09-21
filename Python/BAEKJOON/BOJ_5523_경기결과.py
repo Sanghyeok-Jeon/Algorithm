b3af6b825eebdba13455822ca21d87aa3f06cc01
전상혁 <sh_jeon@kakao.com>
2021-09-21 14:24:21 +0900

    2021-09-21 14:24:21

---
+++ Python/BAEKJOON/BOJ_5523_경기결과.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('BOJ_5523.txt', 'r')
+
+N = int(input())
+winA, winB = 0, 0
+for _ in range(N):
+    scoreA, scoreB = map(int, sys.stdin.readline().split())
+    if scoreA > scoreB:
+        winA += 1
+    elif scoreA < scoreB:
+        winB += 1
+print(winA, winB)
