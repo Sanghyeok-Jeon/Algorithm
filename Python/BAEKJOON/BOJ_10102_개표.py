b88328531a036dc616df062151cdb2c48b7faff9
전상혁 <sh_jeon@kakao.com>
2021-09-28 00:12:00 +0900

    2021-09-28 00:12:00

---
+++ Python/BAEKJOON/BOJ_10102_개표.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('BOJ_10102.txt', 'r')
+
+V = int(input())
+data = input()
+A, B = 0, 0
+for d in data:
+    if d == 'A':
+        A += 1
+    else:
+        B += 1
+
+print('A' if A > B else 'Tie' if A == B else 'B')
