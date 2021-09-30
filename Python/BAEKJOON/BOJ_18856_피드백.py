8655adfb0bff0d6994c5b36768cfe45e2877fece
전상혁 <sh_jeon@kakao.com>
2021-10-01 00:01:26 +0900

    2021-10-01 00:01:26

---
+++ Python/BAEKJOON/BOJ_18856_피드백.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('BOJ_18856.txt', 'r')
+
+N = int(input())
+prime = [0, 0] + [1] * 999
+A = [1]
+
+for i in range(2, 1001):
+    if prime[i]:
+        if len(A) != N:
+            A.append(i)
+        else:
+            break
+        for j in range(2*i, 1001, i):
+            prime[j] = 0
+
+print(N)
+print(*A)
