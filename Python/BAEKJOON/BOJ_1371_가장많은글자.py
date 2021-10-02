6e0d8f9dd2a96fe33d55006a9ec5776c3d215b4c
전상혁 <sh_jeon@kakao.com>
2021-10-03 00:01:12 +0900

    2021-10-03 00:01:12

---
+++ Python/BAEKJOON/BOJ_1371_가장많은글자.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('BOJ_1371.txt', 'r')
+
+data = ''
+while True:
+    try:
+        data += input()
+    except:
+        break
+
+dicCnt = {}
+for d in data:
+    if d == ' ':
+        continue
+
+    if d in dicCnt:
+        dicCnt[d] += 1
+    else:
+        dicCnt[d] = 1
+
+result = [k for k, v in dicCnt.items() if max(dicCnt.values()) == v]
+
+print(''.join(sorted(result)))
