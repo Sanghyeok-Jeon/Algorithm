5c98b5b284cebf82c8641a5cceaa2d1d2382f813
전상혁 <sh_jeon@kakao.com>
2021-10-18 00:09:50 +0900

    2021-10-18 00:09:50

---
+++ Python/BAEKJOON/BOJ_1551_수열의변화.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('BOJ_1551.txt', 'r')
+
+N, K = map(int, input().split())
+nums = list(map(int, input().split(',')))
+for _ in range(K):
+    nums = [nums[i+1]-nums[i] for i in range(len(nums)-1)]
+
+print(*nums, sep=',')
