8971dd3ec7bcaec6cca4be9f44348cf07c8fe908
전상혁 <sh_jeon@kakao.com>
2021-10-19 00:00:38 +0900

    2021-10-19 00:00:38

---
+++ Python/BAEKJOON/BOJ_1568_새.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('BOJ_1568.txt', 'r')
+
+N = int(input())
+K = 1
+cnt = 0
+
+while N > 0:
+    if K > N:
+        K = 1
+    N -= K
+    K += 1
+    cnt += 1
+
+print(cnt)
