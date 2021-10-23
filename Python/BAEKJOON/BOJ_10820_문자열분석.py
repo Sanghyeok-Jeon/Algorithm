f2f593a0c223d3aa3b46f02a6b366523dc813571
전상혁 <sh_jeon@kakao.com>
2021-10-23 23:12:51 +0900

    2021-10-23 23:12:51

---
+++ Python/BAEKJOON/BOJ_10820_문자열분석.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('BOJ_10820.txt', 'r')
+
+while True:
+    str = sys.stdin.readline().rstrip('\n')
+
+    if not str:
+        break
+
+    lst = [0, 0, 0, 0]
+    for s in str:
+        if s == ' ':
+            lst[3] += 1
+        elif s.isdigit():
+            lst[2] += 1
+        else:
+            if s.islower():
+                lst[0] += 1
+            else:
+                lst[1] += 1
+
+    print(*lst)
