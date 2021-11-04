be6b4882ed71703fb352e4a72d4f2f24fa312e11
전상혁 <sh_jeon@kakao.com>
2021-11-04 22:24:53 +0900

    2021-11-04 22:24:53

---
+++ Python/BAEKJOON/BOJ_1977_완전제곱수.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('BOJ_1977.txt', 'r')
+
+M = int(input())
+N = int(input())
+totalSquare = []
+
+for i in range(1, 10001):
+    target = i*i
+    if target > N:
+        break
+
+    if M <= target <= N:
+        totalSquare.append(target)
+
+if len(totalSquare):
+    print(sum(totalSquare), totalSquare[0], sep='\n')
+else:
+    print(-1)
