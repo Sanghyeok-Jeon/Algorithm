0a13f6a110bde734828ed077def7958ce8c94dfe
전상혁 <sh_jeon@kakao.com>
2021-11-11 20:43:55 +0900

    2021-11-11 20:43:55

---
+++ Python/BAEKJOON/BOJ_17202_핸드폰번호궁합.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('BOJ_17202.txt', 'r')
+
+A = input()
+B = input()
+phoneNumber = ''
+for i in range(8):
+    phoneNumber += A[i]
+    phoneNumber += B[i]
+
+while len(phoneNumber) > 2:
+    tmpNum = ''
+    for i in range(len(phoneNumber)-1):
+        tmpNum += str((int(phoneNumber[i])+int(phoneNumber[i+1])) % 10)
+    phoneNumber = tmpNum
+
+print(phoneNumber)
