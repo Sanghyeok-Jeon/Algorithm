7ce25c824921d3d27708055b03f9265f0a3e5375
전상혁 <sh_jeon@kakao.com>
2021-11-13 00:00:13 +0900

    2021-11-13 00:00:13

---
+++ Python/BAEKJOON/BOJ_3181_줄임말만들기.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('BOJ_3181.txt', 'r')
+
+data = list(input().split())
+noList = ['i', 'pa', 'te', 'ni', 'niti', 'a', 'ali', 'nego', 'no', 'ili']
+result = ''
+for i in range(len(data)):
+    if i != 0:
+        if data[i] in noList:
+            continue
+    result += data[i][0]
+
+print(result.upper())
