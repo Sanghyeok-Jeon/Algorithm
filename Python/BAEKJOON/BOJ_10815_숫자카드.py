412e7dbb82a12450a298622bb60d56c0f6d4fc9b
전상혁 <sh_jeon@kakao.com>
2023-09-28 23:12:49 +0900

    2023-09-28 : python

---
+++ Python/BAEKJOON/BOJ_10815_숫자카드.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_10815.txt', 'r')
+
+N = int(input())
+cardNum = set(map(int, input().split()))
+M = int(input())
+cardDummy = list(map(int, input().split()))
+
+result = [0] * M
+for i in range(M):
+    if cardDummy[i] in cardNum:
+        result[i] = 1
+
+print(*result)
---
+++ Python/BAEKJOON/BOJ_14425_문자열집합.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('BOJ_14425.txt', 'r')
+
+N, M = map(int, input().split())
+S = set(input() for _ in range(N))
+
+answer = 0
+for _ in range(M):
+    if input() in S:
+        answer += 1
+
+print(answer)
