462e87443fc6d837f8d93f063bbef008308aacc2
전상혁 <sh_jeon@kakao.com>
2023-10-01 22:26:52 +0900

    2023-10-01 : python

---
+++ Python/BAEKJOON/BOJ_13241_최소공배수.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_13241.txt', 'r')
+
+def GCD(x, y):
+    while y:
+        x, y = y, x%y
+    return x
+
+def LCM(x, y):
+    result = (x*y) // GCD(x, y)
+    return result
+
+A, B = map(int, input().split())
+print(LCM(A, B))
---
+++ Python/BAEKJOON/BOJ_1934_최소공배수.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('BOJ_1934.txt', 'r')
+
+def GCD(x, y):
+    while y:
+        x, y = y, x%y
+    return x
+
+def LCM(x, y):
+    result = (x*y) // GCD(x, y)
+    return result
+
+T = int(input())
+for _ in range(T):
+    A, B = map(int, input().split())
+    print(LCM(A, B))
