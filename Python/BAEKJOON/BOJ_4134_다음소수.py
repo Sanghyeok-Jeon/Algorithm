28fbf7420ec513cafd35cc9d202f8f856a64c3f3
전상혁 <sh_jeon@kakao.com>
2023-10-03 10:22:37 +0900

    2023-10-04 : python algorithm

---
+++ Python/BAEKJOON/BOJ_4134_다음소수.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('BOJ_4134.txt', 'r')
+
+def isPrime(x):
+    if x < 2:
+        return False
+
+    for i in range(2, int(x**0.5)+1):
+        if not x % i:
+            return False
+
+    return True
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+
+    while True:
+        if isPrime(n):
+            print(n)
+            break
+        else:
+            n += 1
