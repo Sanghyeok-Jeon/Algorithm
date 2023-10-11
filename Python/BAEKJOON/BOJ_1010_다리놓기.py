418e5837a8ff4d357a224184e49a416072994a46
전상혁 <sh_jeon@kakao.com>
2023-10-11 22:19:37 +0900

    2023-10-10 : algorithm with python #2

---
+++ Python/BAEKJOON/BOJ_1010_다리놓기.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('BOJ_1010.txt', 'r')
+
+T = int(input())
+for _ in range(T):
+    N, M = map(int, input().split())
+    dp = [[0]*(M+1) for i in range(N+1)]
+    for n in range(N+1):
+        for m in range(n, M+1):
+            if n < 2:
+                dp[n][m] = n*m
+            else:
+                dp[n][m] = dp[n-1][m-1] + dp[n][m-1]
+
+    print(dp[N][M])
