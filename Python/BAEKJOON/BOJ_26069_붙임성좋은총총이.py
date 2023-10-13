66a178fc614664df7f68bda5f08d14c33df6c6e7
전상혁 <sh_jeon@kakao.com>
2023-10-13 09:49:17 +0900

    2023-10-13 : algorithm with python #1

---
+++ Python/BAEKJOON/BOJ_26069_붙임성좋은총총이.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('BOJ_26069.txt', 'r')
+
+input = sys.stdin.readline
+
+N = int(input())
+rainbowDance = set()
+for _ in range(N):
+    A, B = input().split()
+    if A == 'ChongChong' or B == 'ChongChong' or A in rainbowDance or B in rainbowDance:
+        rainbowDance.add(A)
+        rainbowDance.add(B)
+
+print(len(rainbowDance))
