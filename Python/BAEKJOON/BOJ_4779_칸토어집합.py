38c5edfbb00851fa33e8e901dae28ea8831fe167
전상혁 <sh_jeon@kakao.com>
2023-10-16 21:53:55 +0900

    2023-10-16 : algorithm with python #1

---
+++ Python/BAEKJOON/BOJ_4779_칸토어집합.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('BOJ_4779.txt', 'r')
+
+def make_cantorset(s, length, start):
+    if length == 1:
+        return
+
+    length //= 3
+    end = start + length + length
+    for i in range(start+length, end):
+        s[i] = ' '
+
+    make_cantorset(s, length, start)
+    make_cantorset(s, length, end)
+
+lines = sys.stdin.readlines()
+for line in lines:
+    N = int(line)
+    cantorSet = ['-' for _ in range(3**N)]
+    make_cantorset(cantorSet, len(cantorSet), 0)
+    print(''.join(cantorSet))
