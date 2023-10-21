7b1019b7dd092f331ecc22b26708869a0ad790b4
전상혁 <sh_jeon@kakao.com>
2023-10-21 22:46:08 +0900

    2023-10-21 : algorithm with python #1

---
+++ Python/BAEKJOON/BOJ_1904_01타일.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('BOJ_1904.txt', 'r')
+
+N = int(input())
+tile = [0] * (N+1)
+
+for i in range(1, N+1):
+    if i == 1:
+        tile[i] = 1
+    elif i == 2:
+        tile[i] = 2
+    else:
+        tile[i] = ((tile[i-1] % 15746) + (tile[i-2] % 15746)) % 15746
+
+print(tile[N])
