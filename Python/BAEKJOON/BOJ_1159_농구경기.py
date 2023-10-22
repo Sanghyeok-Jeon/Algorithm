c71dfe1f7ff886f16380f00d02d75209da772905
전상혁 <sh_jeon@kakao.com>
2023-10-23 00:42:19 +0900

    2023-10-23 : algorithm with python #1

---
+++ Python/BAEKJOON/BOJ_1159_농구경기.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('BOJ_1159.txt', 'r')
+
+N = int(input())
+nameFrontTwo = {}
+for _ in range(N):
+    name = sys.stdin.readline()[0]
+    if name in nameFrontTwo:
+        nameFrontTwo[name] += 1
+    else:
+        nameFrontTwo[name] = 1
+
+result = []
+for k, v in nameFrontTwo.items():
+    if v >= 5:
+        result.append(k)
+
+if result:
+    print(''.join(sorted(result)))
+else:
+    print('PREDAJA')
