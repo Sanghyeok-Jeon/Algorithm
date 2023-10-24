b9ccaa4d39d340bd2b775f31572067d228ca051c
전상혁 <sh_jeon@kakao.com>
2023-10-24 23:25:45 +0900

    2023-10-24 : algorithm with python #1

---
+++ Python/BAEKJOON/BOJ_11659_구간합구하기4.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('BOJ_11659.txt', 'r')
+
+N, M = map(int, input().split())
+number = list(map(int, sys.stdin.readline().split()))
+
+prefixSum = [0]
+tmpSum = 0
+for num in number:
+    tmpSum += num
+    prefixSum.append(tmpSum)
+
+for _ in range(M):
+    i, j = map(int, sys.stdin.readline().split())
+    print(prefixSum[j] - prefixSum[i-1])
