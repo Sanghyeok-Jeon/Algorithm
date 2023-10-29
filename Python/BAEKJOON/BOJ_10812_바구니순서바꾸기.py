b7a5b8ccfab245d79d85c5c243dfe649144d686f
전상혁 <sh_jeon@kakao.com>
2023-10-29 22:16:06 +0900

    2023-10-29 : algorithm with python #1

---
+++ Python/BAEKJOON/BOJ_10812_바구니순서바꾸기.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('BOJ_10812.txt', 'r')
+
+N, M = map(int, input().split())
+basket = list(range(1, N+1))
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    i, j, k = i-1, j-1, k-1
+    basket = basket[:i] + basket[k:j+1] + basket[i:k] + basket[j+1:]
+
+print(*basket)
