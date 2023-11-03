3fd9b1634ba101f3e018492c5a3cf08fc4784914
전상혁 <sh_jeon@kakao.com>
2023-11-03 22:53:49 +0900

    2023-11-03 : algorithm with python #1

---
+++ Python/BAEKJOON/BOJ_1919_애너그램만들기.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('BOJ_1919.txt', 'r')
+
+str1 = input()
+str2 = input()
+
+cntChar = [0] * 26
+for s in str1:
+    cntChar[ord(s) - 97] += 1
+for s in str2:
+    cntChar[ord(s) - 97] -= 1
+
+total = 0
+for c in cntChar:
+    total += abs(c)
+
+print(total)
