f3a6625c4f7a4db23b49cc34af32da26b34b1261
전상혁 <sh_jeon@kakao.com>
2023-12-18 21:36:10 +0900

    2023-12-18 : algorithm with python #1

--- Python/BAEKJOON/BOJ_3360.py
+++ Python/BAEKJOON/BOJ_3360.py
@@ -1,23 +1,7 @@
-import sys
-sys.stdin = open('BOJ_3360.txt', 'r')
-
 n = int(input())
 
-dp = [[0, 0, 0] for _ in range(n+1)]
+cnt = 0
+for i in range(n // 3 + 1):
+    cnt += (n - 3*i) // 2 + 1
 
-for i in range(1, n+1):
-    if i == 1:
-        dp[i][0] = 1
-    elif i == 2:
-        dp[i][0] = dp[i-1][0]
-        dp[i][1] = 1
-    elif i == 3:
-        dp[i][0] = dp[i-1][0]
-        dp[i][1] = dp[i-2][0] + dp[i-2][1]
-        dp[i][2] = 1
-    else:
-        dp[i][0] = dp[i-1][0] % 1000000
-        dp[i][1] = (dp[i-2][0] + dp[i-2][1]) % 1000000
-        dp[i][2] = (dp[i-3][0] + dp[i-3][1] + dp[i-3][2]) % 1000000
-
-print(sum(dp[n]) % 1000000)
+print(cnt % 1000000)
